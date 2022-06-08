2831ad5e552829e8bac4eeaad1b908fae8186d4b
gustavo.ggoncalves1 <gustavo.ggoncalves1@SBC0505243W10-1>
2022-06-08 17:19:08 -0300

    Adicionar arquivos de projeto.

---
+++ PessoaTI14T/DAOPessoa.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace PessoaTI14T
+{
+    class DAOPessoa
+    {
+        public MySqlConnection conexao;
+        public string dados;
+        public string comando;
+        public string resultado;
+        public int contador;
+        public string msg;
+        public int i;
+        public int[] vetorCodigo;// vetor Codigo
+        public long[] vetorCpf;// vetor CPF
+        public string[] vetorNome;// vetor nome
+        public string[] vetorTelefone;// vetor telefone
+        public string[] vetorEndereco;// vetor endereço
+
+        public DAOPessoa()
+        {
+            conexao = new MySqlConnection("server=localhost;dataBase=turma14t;Uid=root;password=;");
+            try
+            {
+                conexao.Open();
+                MessageBox.Show("Conectado com sucesso!!");// mostra que esta conectado
+            }
+            catch (Exception e)
+            {
+                 MessageBox.Show(" algo deu errado\n\n " + e);// mostra o erro em tela
+                conexao.Close();// fecha a conexão com banco de dados 
+            }
+
+        }// fim do DAOPessoa
+
+        public void Inserir(long cpf, string nome, string telefone, string edereco)
+        {
+            try
+            {
+                dados = "('', '" + cpf + "', '" + nome + "', '" + telefone + "', '" + edereco + "')";
+                comando = "Insert into PessoaTI14T(codigo, cpf, nome, telefone, edereco) values " + dados;
+
+                MySqlCommand sql = new MySqlCommand(comando, conexao);
+                resultado = " " + sql.ExecuteNonQuery();// Executa o insert no BD
+                MessageBox.Show(resultado + " Linhas Afetadas ");
+
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(" algo deu errado\n\n " + e);
+
+            }
+
+        }// Fim do método inserir
+
+        public void Preenchervetor()
+        {
+            string query = " select * from PessoaTI14T ";
+            // instanciar
+            vetorCodigo = new int[100];
+            vetorCpf = new long[100];
+            vetorNome = new string[100];
+            vetorTelefone = new string[100];
+            vetorEndereco = new string[100];
+            
+            //preencher com valores inciciais 
+            for (i = 0; i < 100; i++)
+            {
+                vetorCodigo[i] = 0;
+                vetorCpf[i] = 0;
+                vetorNome[i] = "";
+                vetorTelefone[i] = "";
+                vetorEndereco[i] = "";
+                
+
+            }//fim do for
+             //Creando o consulta do BD-Banco de dados 
+
+            MySqlCommand coletar = new MySqlCommand(query, conexao);
+
+            //Leitura do banco de dados 
+
+            MySqlDataReader leitura = coletar.ExecuteReader();
+
+            i = 0;
+            contador = 0;
+
+            //Preencher os valores com dados do banco de dados
+
+            while (leitura.Read())//Enquanto For verdadeiro, executa o que esta no While
+            {
+                vetorCodigo[i] = Convert.ToInt32(leitura["codigo"]);
+                vetorCpf[i] = Convert.ToInt64(leitura["cpf"]);
+                vetorNome[i] = leitura["Nome"] + "";
+                vetorTelefone[i] = leitura["telefone"] + "";
+                vetorEndereco[i] = leitura["edereco"] + "";
+                i++;
+                contador++;
+
+            }// fim do while
+
+            // fecha a leitura do banco de dados
+
+            leitura.Close();
+
+
+        }//fim do medoto de preencher o vetor
+
+        // metodo de consulta de todos banco de dados
+
+        public string Consultar()
+        {
+            //preencher o ventor
+            Preenchervetor();
+
+            msg = "";
+
+            for (i = 0; i < contador; i++)
+            {
+
+                msg += " codigo " + vetorCodigo[i] +
+                       "  cpf " + vetorCpf[i] +
+                       " nome " + vetorNome[i] +
+                       " telefone " + vetorTelefone[i] +
+                       "  endereco " + vetorEndereco[i] +
+                       "\n\n ";
+
+            }// fim do for
+
+            return msg;
+
+        }// fim do metodo de consulta
+
+        public long ConsultarCpf(int cod)
+        {
+            Preenchervetor();
+            for(i=0; i<contador; i++)
+            { 
+                if (vetorCodigo[i] == cod)
+                {
+                    return vetorCpf[i];
+                }//fim do if
+            }
+            return -1;
+        }//Fim consultar cpf
+
+        public string ConsultarNome(int cod)
+        {
+            Preenchervetor();
+            for(i=0; i<contador; i++)
+            {
+                if (vetorCodigo[i] == cod)
+                {
+                    return vetorNome[i];
+                }
+
+            }
+            return "Nome Não encontrado";
+        }// Fim  do consultar Nome
+         public string ConsultarTelefone(int cod)
+        {
+            Preenchervetor();
+            for(i=0; i<contador; i++)
+            {
+                if(vetorCodigo[i] == cod)
+                {
+                    return vetorTelefone[i];
+                }
+
+            }
+            return " telefone Não encontrado";
+        }//Fim do Consultar telefone
+
+        public string ConsultarEndereco(int cod)
+        {
+            Preenchervetor();
+            for (i = 0; i < contador; i++)
+            {
+                if (vetorCodigo[i] == cod)
+                {
+                    return vetorEndereco[i];
+                }
+
+            }
+            return "Endereço Não encontrado";
+        }// Fim  do consultar Nome
+
+        public void Atualizar (int cod,string campo, string novoDado)
+        {
+            try
+            {
+                string query = "update pessoaTI14T set" + campo + " = '" + novoDado + "' where codigo = '" + cod + "'";
+                MySqlCommand sql = new MySqlCommand(query, conexao);
+                string resultado = "" + sql.ExecuteNonQuery();
+                MessageBox.Show(resultado + " linha afetada!");
+
+            }
+            catch(Exception erro)
+            {
+                MessageBox.Show("" + erro);
+            }
+
+        }// fim do atualizar
+        public void Deletar(int cod)
+        {
+            try
+            { 
+                string query = "update pessoaTI14T set = '" + cod + "'";
+                MySqlCommand sql = new MySqlCommand(query, conexao);
+                string resultado = "" + sql.ExecuteNonQuery();
+                MessageBox.Show(resultado + " linha afetada!");
+
+            }
+            catch (Exception erro)
+            {
+
+                MessageBox.Show("" + erro);
+
+            }
+        }//fim do deletar
+
+
+    }// fim da classe 
+
+
+}//fim do projeto
---
+++ PessoaTI14T/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PessoaTI14T
+{
+    public partial class Form1 : Form
+    {
+
+        DAOPessoa pessoa;
+        public Form1()
+        {
+            InitializeComponent();
+            pessoa = new DAOPessoa();// Abrindo a conexão com o banco de dados
+        }// fim do construtor
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }//Fim do botão Deletar
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }//fim do Atualizar
+
+        private void Cadatrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //int codigo = Convert.ToInt32(textBox1.Text);// coletar o campo
+                string tratamentoCPF = maskedTextBox1.Text.Replace(",", "");
+                tratamentoCPF = tratamentoCPF.Replace("-", "");
+                long cpf = Convert.ToInt64(tratamentoCPF);// coletar o campo cpf
+                string nome = textBox2.Text;// coletar o campo
+                string telefone = maskedTextBox2.Text;// coletar o campo
+                string edereco = textBox4.Text;// coletar o campo
+                // chamar o metodo inserir 
+                pessoa.Inserir(cpf, nome, telefone, edereco);//Imserir no banco de dados do formulario
+            }
+            catch (Exception erro)
+            {
+
+                MessageBox.Show("" + erro);
+            }
+
+
+
+        }//Fim do botão Cadastrar
+
+        private void Consultar_Click(object sender, EventArgs e)
+        {
+
+            maskedTextBox1.Text = "" + pessoa.ConsultarCpf(Convert.ToInt32(textBox1.Text));
+            textBox2.Text = pessoa.ConsultarNome(Convert.ToInt32(textBox1.Text));
+            maskedTextBox2.Text = "" + pessoa.ConsultarTelefone(Convert.ToInt32(textBox1.Text));
+            textBox4.Text = pessoa.ConsultarEndereco(Convert.ToInt32(textBox1.Text));
+
+        }//fim do botão consultar
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }//fim textBox Codigo
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }// maskedTextBox CPF
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }// TextBox Nome
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }//MaskedTextBox Telefone
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }//TextBox Endereço
+    }// Fim da classe 
+}//Fim do projeto
