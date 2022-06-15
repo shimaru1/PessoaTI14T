1b7d86c79f2c39b4117ec9b704c8009f82749117
Gustavo_gomes <103595240+shimaru1@users.noreply.github.com>
2022-06-15 15:04:40 -0300

    Add files via upload

---
+++ PessoaTI14T/PessoaTI14T/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace PessoaTI14T
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.Cadatrar = new System.Windows.Forms.Button();
+            this.Deletar = new System.Windows.Forms.Button();
+            this.Consultar = new System.Windows.Forms.Button();
+            this.Atualizar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pessoa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Código:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CPF:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Endereço:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(8, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Telefone:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(8, 174);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 19);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Nome:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(84, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 25);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(84, 174);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(343, 25);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(83, 249);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(344, 25);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(84, 137);
+            this.maskedTextBox1.Mask = "999.999.999-99";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(343, 25);
+            this.maskedTextBox1.TabIndex = 10;
+            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox2.Location = new System.Drawing.Point(84, 213);
+            this.maskedTextBox2.Mask = "(99)99999-9999";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(343, 25);
+            this.maskedTextBox2.TabIndex = 11;
+            this.maskedTextBox2.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox2_MaskInputRejected);
+            // 
+            // Cadatrar
+            // 
+            this.Cadatrar.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cadatrar.Location = new System.Drawing.Point(12, 309);
+            this.Cadatrar.Name = "Cadatrar";
+            this.Cadatrar.Size = new System.Drawing.Size(95, 50);
+            this.Cadatrar.TabIndex = 12;
+            this.Cadatrar.Text = "Cadatrar";
+            this.Cadatrar.UseVisualStyleBackColor = true;
+            this.Cadatrar.Click += new System.EventHandler(this.Cadatrar_Click);
+            // 
+            // Deletar
+            // 
+            this.Deletar.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Deletar.Location = new System.Drawing.Point(349, 309);
+            this.Deletar.Name = "Deletar";
+            this.Deletar.Size = new System.Drawing.Size(95, 50);
+            this.Deletar.TabIndex = 13;
+            this.Deletar.Text = "Deletar";
+            this.Deletar.UseVisualStyleBackColor = true;
+            this.Deletar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Consultar
+            // 
+            this.Consultar.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Consultar.Location = new System.Drawing.Point(124, 309);
+            this.Consultar.Name = "Consultar";
+            this.Consultar.Size = new System.Drawing.Size(95, 50);
+            this.Consultar.TabIndex = 14;
+            this.Consultar.Text = "Consultar";
+            this.Consultar.UseVisualStyleBackColor = true;
+            this.Consultar.Click += new System.EventHandler(this.Consultar_Click);
+            // 
+            // Atualizar
+            // 
+            this.Atualizar.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Atualizar.Location = new System.Drawing.Point(239, 309);
+            this.Atualizar.Name = "Atualizar";
+            this.Atualizar.Size = new System.Drawing.Size(95, 50);
+            this.Atualizar.TabIndex = 15;
+            this.Atualizar.Text = "Atualizar";
+            this.Atualizar.UseVisualStyleBackColor = true;
+            this.Atualizar.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(456, 389);
+            this.Controls.Add(this.Atualizar);
+            this.Controls.Add(this.Consultar);
+            this.Controls.Add(this.Deletar);
+            this.Controls.Add(this.Cadatrar);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.MaximumSize = new System.Drawing.Size(476, 432);
+            this.MinimumSize = new System.Drawing.Size(476, 432);
+            this.Name = "Form1";
+            this.Text = "Tela Precipal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Button Cadatrar;
+        private System.Windows.Forms.Button Deletar;
+        private System.Windows.Forms.Button Consultar;
+        private System.Windows.Forms.Button Atualizar;
+    }
+}
+
---
+++ PessoaTI14T/PessoaTI14T/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PessoaTI14T
+{
+    public partial class Form1 : Form
+    {
+
+        DAOPessoa pessoa;
+        public Form1()
+        {
+            InitializeComponent();
+            pessoa = new DAOPessoa();// Abrindo a conexão com o banco de dados
+            textBox1.Text =Convert.ToString(pessoa.ConsultarCodigo() + 1);
+            textBox1.ReadOnly = true;
+        }// fim do construtor
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AtivaCampos();
+            pessoa.Deletar(Convert.ToInt32(textBox1.Text));
+        }//Fim do botão Deletar
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            AtivarTodosCampos();
+            if (textBox2.Text == "")
+            {
+                maskedTextBox1.Text = "" + pessoa.ConsultarCpf(Convert.ToInt32(textBox1.Text));
+                textBox2.Text = pessoa.ConsultarTelefone(Convert.ToInt32(textBox1.Text));
+                maskedTextBox2.Text = pessoa.ConsultarNome(Convert.ToInt32(textBox1.Text));
+                textBox4.Text = pessoa.ConsultarEndereco(Convert.ToInt32(textBox1.Text));
+
+            }
+            else
+            {
+                string atuCPF = pessoa.Atualizar(Convert.ToInt32(textBox1.Text), "CPF", TrataCPF(maskedTextBox1.Text));
+                // atualizar Nome
+                string atuNome = pessoa.Atualizar(Convert.ToInt32(textBox1.Text), "nome", textBox2.Text);
+                //atualizar telefone
+                string atuTelefone = pessoa.Atualizar(Convert.ToInt32(textBox1.Text), "telefone", maskedTextBox2.Text);
+                //atualizar endereco
+                string atuEndereco = pessoa.Atualizar(Convert.ToInt32(textBox1.Text), "endereco", textBox4.Text);
+                //Resposta...
+                if((atuCPF == "Atualizado") && (atuNome == "Atualizado") && (atuTelefone == "Atualizado") && (atuEndereco == "Atualizado"))
+                {
+                    MessageBox.Show("Atualizado com Sucesso!");
+                }
+                else
+                {
+                    MessageBox.Show("Não Atualizado!");
+                }
+                Limpar();// Limpa Campo
+               
+            }
+
+        }//fim do Atualizar
+
+        public void AtivaCampos()
+        { 
+          
+                textBox1.ReadOnly = false;//Codigo
+                maskedTextBox1.ReadOnly = true;//CPF
+                textBox2.ReadOnly = true;//nome
+                maskedTextBox2.ReadOnly = true;//telefone
+                textBox4.ReadOnly = true;//Endereço
+         
+        }
+        public void inativarCompos()
+        {
+
+            textBox1.ReadOnly = true;//Codigo
+            maskedTextBox1.ReadOnly = false;//CPF
+            textBox2.ReadOnly = false;//Nome
+            maskedTextBox2.ReadOnly = false;//Telefone
+            textBox4.ReadOnly = false;//Endereço
+
+        }
+        public void AtivarTodosCampos()
+        {
+            textBox1.ReadOnly = false;//Codigo
+            maskedTextBox1.ReadOnly = false;//CPF
+            textBox2.ReadOnly = false;//Nome
+            maskedTextBox2.ReadOnly = false;//Telefone
+            textBox4.ReadOnly = false;//Endereço
+
+        }
+
+        private void Cadatrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(textBox1.ReadOnly == false)
+                {
+                    Limpar();                    
+                    inativarCompos();
+                }
+                else
+                {
+
+                
+                    //int codigo = Convert.ToInt32(textBox1.Text);// coletar o campo
+                    string tratamentoCPF = maskedTextBox1.Text.Replace(",", "");
+                    tratamentoCPF = tratamentoCPF.Replace("-", "");
+                    long cpf = Convert.ToInt64(tratamentoCPF);// coletar o campo cpf
+                    string nome = textBox2.Text;// coletar o campo
+                    string telefone = maskedTextBox2.Text;// coletar o campo
+                    string edereco = textBox4.Text;// coletar o campo
+                     // chamar o metodo inserir 
+                pessoa.Inserir(cpf, nome, telefone, edereco);//Imserir no banco de dados do formulario
+ 
+                Limpar();
+                }
+            }
+            catch (Exception erro)
+            {
+
+                MessageBox.Show("" + erro);
+            }
+
+
+
+        }//Fim do botão Cadastrar
+
+        public long TrataCPF(string cpf)
+        {
+            string tratamento = cpf.Replace(",", "");
+            tratamento = tratamento.Replace("-", "");
+            return Convert.ToInt64(tratamento);
+
+        }// fim tratamento do Cpf
+        public void Limpar()
+        {
+            textBox1.Text = " " + pessoa.ConsultarCodigo();
+            maskedTextBox1.Text = "";//Cpf
+            textBox2.Text = "";// nome
+            maskedTextBox2.Text = "";//
+            textBox4.Text = "";// endereço
+
+            
+        }
+
+        private void Consultar_Click(object sender, EventArgs e)
+        {
+            if(textBox1.ReadOnly == true)
+            {
+                AtivaCampos();
+
+            }
+            else 
+            { 
+                maskedTextBox1.Text = "" + pessoa.ConsultarCpf(Convert.ToInt32(textBox1.Text));
+                textBox2.Text = pessoa.ConsultarNome(Convert.ToInt32(textBox1.Text));
+                maskedTextBox2.Text = "" + pessoa.ConsultarTelefone(Convert.ToInt32(textBox1.Text));
+                textBox4.Text = pessoa.ConsultarEndereco(Convert.ToInt32(textBox1.Text));
+            }
+           
+        }//fim do botão consultar
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.ReadOnly = true;
+        }//fim textBox Codigo
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }// maskedTextBox CPF
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }// TextBox Nome
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }//MaskedTextBox Telefone
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }//TextBox Endereço
+    }// Fim da classe 
+}//Fim do projeto
